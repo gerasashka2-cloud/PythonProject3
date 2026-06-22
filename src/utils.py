a262f0aa15b0e6f9d2b28e83a66b3813d2d871d5
Alexander <gera.sashka2@gmail.com>
2026-06-22 19:52:27 +0300

    added logging utils.py and masks.py

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,22 @@
 import json
+import logging
+
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_hendler = logging.FileHandler("./logs/utils.log" 'w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_hendler.setFormatter(file_formatter)
+logger.addHandler(file_hendler)
 
 
 def load_operations(file_path: str = "data/operations.json") -> dict:
     """ Загружает транзакции из JSON-файла """
     try:
+        logger.info("Загрузка файла операций...")
         with open(file_path, "r", encoding="utf-8") as f:
             operations = json.load(f)
         return operations  # type: ignore
     except (FileNotFoundError, ValueError, json.JSONDecodeError):
+        logger.error("Произошла ошибка при загрузке файла операций")
         return []  # type: ignore
